e8fe35764e8a4b3e8aba55736b753bff17c1bc71
AppaNow <polatnowak@gmail.com>
2020-11-21 18:30:05 +0100

    Set ADoor defult meshes

--- Source/PortalGame/Door.cpp
+++ Source/PortalGame/Door.cpp
@@ -9,11 +9,16 @@ ADoor::ADoor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> FrameAsset(TEXT("StaticMesh'/Game/StarterContent/Props/SM_DoorFrame.SM_DoorFrame'"));
 	FrameMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("FrameMeshComponent");
+	FrameMeshComponent->SetStaticMesh(FrameAsset.Object);
 	RootComponent = FrameMeshComponent;
 
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> DoorAsset(TEXT("StaticMesh'/Game/StarterContent/Props/SM_Door.SM_Door'"));
 	DoorMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("DoorMeshComponent");
 	DoorMeshComponent->SetupAttachment(FrameMeshComponent);
+	DoorMeshComponent->SetStaticMesh(DoorAsset.Object);
+	DoorMeshComponent->SetRelativeLocation(FVector(0.0f, 45.0f, 0.0f));
 }
 
 // Called when the game starts or when spawned
